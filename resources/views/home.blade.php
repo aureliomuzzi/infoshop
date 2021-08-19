7f89f74bd33adf57ff2e81f8dddeb448b32b6cb6
Aurelio <aurelio@flexpeak.com.br>
2021-08-19 16:32:14 -0400

    Configurado o AdminLTE 3

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('adminlte::page')
+
+@section('title', 'Início')
+
+@section('content_header')
+<h1 class="m-0 text-dark"><i class="fas fa-home"></i> Início
+    <small class="text-muted">- Bem-vindo ao sistema InfoShop - Loja de Informática</small>
+</h1>
+@stop
+
+@section('content')
+    <div class="row">
+
+    </div>
+@stop
