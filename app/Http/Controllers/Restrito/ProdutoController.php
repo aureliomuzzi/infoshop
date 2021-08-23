04b686ee86c67d7e27228dca62ba207d86cf9fa0
Marcelo <marcelodossantos732@yhaoo.com.br>
2021-08-23 17:33:05 -0400

    rotas e tabela de produtos
    criação das rotas do CRUD e criação da tabela produtos.

---
+++ app/Http/Controllers/Restrito/ProdutoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index()
+{
+    
+}
+
+public function create()
+{
+    
+}
+
+public function store()
+{
+    
+}
+
+public function edit()
+{
+
+}
+
+public function update()
+{
+
+}
+
+public function delete()
+{
+
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Restrito\CategoriaController;
+use App\Http\Controllers\Restrito\ProdutoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,14 @@ Route::get('/', [App\Http\Controllers\Publico\HomeController::class, 'index'])->
 
 Route::middleware('auth')->group(function() {
     Route::get('/restrito', [App\Http\Controllers\Restrito\HomeController::class, 'home'])->name('restrito.home');
-    Route::resource('categorias', CategoriaController::class);
+    Route::resource('categorias', CategoriaController::class);  
 });
+
+Route::get('produtos', [ProdutoController::class, 'index']); // listar produtos
+Route::get('produtos/create', [ProdutoController::class, 'create']); // criação
+Route::post('produtos', [ProdutoController::class, 'store']); // salvar produto 
+Route::get('produtos/edit/{produto}', [ProdutoController::class, 'edit']); // edição
+Route::put('produtos/{produto}', [ProdutoController::class, 'update']); // salvar edição
+Route::delete('produtos/{produto}', [ProdutoController::class, 'delete']); // excluir registro
+
+
